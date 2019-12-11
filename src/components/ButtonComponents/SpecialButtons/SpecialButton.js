05bf335bb7e42d9aa87bc03a8a1b02581ab6abb1
IsabellaGuo <isabella.guofei@gmail.com>
2019-12-10 16:01:40 -0800

    step 4 is done

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { tsPropertySignature } from "@babel/types";
 
 const SpecialButton = () => {
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button>
+        {tsPropertySignature.text}
+      </button>
     </>
   );
 };
+
+export default SpecialButton;
