c358c5a3f8dbabbf4743bd4feb0b2e96f5a85548
IsabellaGuo <isabella.guofei@gmail.com>
2019-12-10 21:36:02 -0800

    finish

--- src/App.js
+++ src/App.js
@@ -16,12 +16,30 @@ function App() {
   // Your functions should accept a parameter of the the item data being displayed to the DOM (ie - should recieve 5 if the user clicks on
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
+const [showValue, setShowValue] = useState('');
 
+console.log(showValue);
+console.log(setShowValue);
+
+const addNumber = (number) => {
+  setShowValue(showValue=> showValue + number);
+};
+const addOperator = (operator) => {
+  if (operator === '='){
+    setShowValue(showValue => eval(showValue));
+  } else{
+    setShowValue(showValue => showValue + '' + operator + '');
+  }
+}
   return (
     <div className="container">
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Display number={showValue} />
+        <Numbers addNumber={addNumber} />
+        <Operators addOperator={addOperator} />
+        <Specials />
       </div>
     </div>
   );
